fcc36879e118eaf15ea2f92085ceb786150ccb72
mriamz <143622067+mriamz@users.noreply.github.com>
2023-10-27 09:03:01 -0600

    Add files via upload

--- Unidad4.java
+++ Unidad4.java
@@ -4,6 +4,8 @@
  */
 package unidad4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
@@ -15,47 +17,47 @@ public class Unidad4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //declaracion de arreglos 
-        //no hay nada reservado
-        //elementos con llaves y separando por comas 
-        boolean[] banderas={true,false,true,true,false };
-        String[] nombres = {"cesar","mateo","julio","victor","panchito","cris","liz","fatima","carlos","jacky","josue"};//solo a los string se le tiene que colocar comillas 
-        //cesar,mateo,julio,victor,panchito,cris,liz,fatima,carlos,jacky,josue
-        double[] calificaciones={9.4,8.9,7.8,6.8,9.0,
-                                 7.9,8.2,6.1,8.3,7.6};
-        char[] vocales;//caracteres #,@,a
+        Scanner lectura =new Scanner(System.in);
+        // declaracion del arreglo
+        String opc ;
+        int t;
+       double [] op = new double[7];
+      op[0]=20.3;
+       op[1]=40.6;
+       op[2]=78.6;
+       op[3]=94.7;
+       op[4]=55.3;
+       op[5]=22.6;
+       op[6]=84.3;
+       
+       do{
+           System.out.println("ingresee la operaseion que desea ");
+           System.out.println("1.-sumar ");
+           System.out.println("2.-multiplicar ");
+           System.out.println("3.-salir");
+           t=lectura.nextInt();
+           switch(t){
+               case 1:
+                   double suma=0;
+                   for (int i = 0; i < op.length; i++) {
+                       suma+=op[i];
+                       System.out.println("resutado "+suma);
+                       
+                   }
+                   break;
+               case 2:
+                   double multiplicacion=1;
+                   for (int j = 0; j < op.length; j++) {
+                       multiplicacion+=op[j];
+                       System.out.println("resultado"+multiplicacion);
+                       
+                   }
+                  break;
+           
+           }
+       }while(t!=3);
+       
         
-                //. length es el tamaño de el arreglo
-                System.out.println("banderas - "+banderas[0]);//corresponde al boolean
-                System.out.println("arr -banderas en su posisio 4"
-                        + "-elemento "+banderas[4]);
-                System.out.println("nombres - "+ nombres.length);
-                 System.out.println("nombres - "+ nombres[1]);
-                 System.out.println("nombres - "+ nombres[9]);
-                 System.out.println("nombres - "+ nombres[5]);
-                 System.out.println("nombres - "+ nombres[0]);
-                 System.out.println("calificaciones en su posicion 0"
-                 +calificaciones[0]);
-                  System.out.println("calificaciones en su posicion 1"
-                 +calificaciones[1]);
-                   System.out.println("calificaciones en su posicion 2"
-                 +calificaciones[2]);
-                  System.out.println("calificaciones en su posicion 3"
-                 +calificaciones[3]);
-                   System.out.println("calificaciones en su posicion 4"
-                 +calificaciones[4]);
-                    System.out.println("calificaciones en su posicion 5"
-                 +calificaciones[5]);
-                     System.out.println("calificaciones en su posicion 6"
-                 +calificaciones[6]);
-                      System.out.println("calificaciones en su posicion 7"
-                 +calificaciones[7]);
-                       System.out.println("calificaciones en su posicion 8"
-                 +calificaciones[8]);
-                        System.out.println("calificaciones en su posicion 9"
-                 +calificaciones[9]);
-               // System.out.println(nombres[0]);
-                
     }
     
 }
